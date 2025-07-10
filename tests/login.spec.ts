c920a99ac2f894efb5696a9a0e7c4c60a65723a6
Honeyleth Agbanlog <haagbanlog@gmail.com>
2025-07-10 23:25:36 +0800

    update login spec

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,3 +10,12 @@ test('Verify that user is able to login with valid credentials', async ({ page }
     await page.locator('[data-test="login-button"]').click();
     await expect(page.getByText('Swag Labs')).toBeVisible();
 });
+test('Verify that user is not able to login with invalid credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').fill('standarduser');
+
+    await page.locator('[data-test="password"]').fill('secretsauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
+});
+    
